dad956d1b8806e5d4141c19299d068f9c8d80022
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-03 10:36:14 -0500

    complete all cohorts with 18 or more students query

---
+++ 3_queries_group-by/3_all_cohorts_18_or_more_students.sql
@@ -0,0 +1,7 @@
+/* all cohorts with 18 or more students */
+SELECT cohorts.name AS cohort_name, count(students.*) AS student_count
+FROM cohorts
+JOIN students ON students.cohort_id = cohorts.id
+GROUP BY cohort_name
+HAVING count(students.*) >= 18
+ORDER BY student_count
