e038efe8aff9f7e7a29ad3943e4a8f1ec0096a76
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-03 11:33:26 -0500

    complete enrolled students average assignment completion time query

---
+++ 3_queries_group-by/5_current_students_average_assignment_completion_time.sql
@@ -0,0 +1,7 @@
+/* Get currently enrolled students' average assignment completion time. */
+SELECT students.name AS student, avg(assignment_submissions.duration) AS average_assignment_duration
+FROM students
+JOIN assignment_submissions ON assignment_submissions.student_id = students.id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+ORDER BY average_assignment_duration DESC
